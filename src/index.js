88f1ff3acb177c5dec656358faf82bd0f0794f59
talesbaz <talesbaz@gmail.com>
2019-01-05 15:28:49 -0200

    add eslint and fix lints

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-<BrowserRouter>
+  <BrowserRouter>
     <App />
-  </BrowserRouter>, document.getElementById('root'))
+  </BrowserRouter>, document.getElementById('root'));
